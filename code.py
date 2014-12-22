c17385153d843eb513ae334e81fe7d0d4e534b3f
Filip Smets <alemaaltevinden@gmail.com>
2014-12-22 18:14:27 -0500

    "u fck1n w0t m7"

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
-print("Welcome to democratic programming")
-print("You are one of the first, enjoy :)")
-print("test on line 3")
-for i in range(420):
+# This program sorts an array
+# Written by the collective minds of http://squarific.com/DemocraticProgramming/
+# License: MIT
+unsorted_array = [98, 23, -1, 48, 109, 20]
+def sort_array (array):
+    array = array[:] # Copies the array
+    array.sort()
+    return array
+sorted_array = sort_array(unsorted_array)
