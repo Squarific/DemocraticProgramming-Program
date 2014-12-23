f38a7fe7f706bfb91b002040c95538fa689b33d9
Filip Smets <alemaaltevinden@gmail.com>
2014-12-23 15:17:04 -0500

    "false"

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 # This program sorts an array
 # Written by the collective minds of http://squarific.com/DemocraticProgramming/
-# License: MIT
+# License: MIT, although WTFPL is cooler
 unsorted_array = [98, 23, -1, 48, 109, 20]
 def sort_array (array):
     array = array[:] # Copies the array
