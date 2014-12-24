45ef3c2f25ffdc3907a00ba7a18f490cef3b7227
Filip Smets <alemaaltevinden@gmail.com>
2014-12-24 09:20:03 -0500

    "First try at the menu"

--- code.py
+++ code.py
@@ -1,12 +1,13 @@
 // Cool stuff
+var quit = false;
 while (!quit) {
     var inputString = prompt("What do you want to do?");
     var inputArray = inputString.split(" ");
 
     if (typeof commandos[inputArray[0]] == "function") {
-        commandos[inputArray[0]].apply(commandos, inputArray);
+        quit = commandos[inputArray[0]].apply(commandos, inputArray);
     }
 }
 
 commandos = {
-false
+}
