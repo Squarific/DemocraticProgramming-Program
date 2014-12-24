b62ecf624d9b4ba4377b105bd0f9a68347b4f54f
Filip Smets <alemaaltevinden@gmail.com>
2014-12-24 07:28:28 -0500

    "Woot"

--- code.py
+++ code.py
@@ -1,12 +1,11 @@
-# This program sorts an array
-# Written by the collective minds of http://squarific.com/DemocraticProgramming/
-# License: MIT, although WTFPL is cooler
-unsorted_array = [98, 23, -1, 48, 109, 20]
-def sort_array (array):
-    array = array[:] # Copies the array
-    for a in range(len(array) - 1, 0, -1):
-        for b in range(a):
-            if array[b] > array[b + 1]:
-                array[b], array[b + 1] = array[b + 1], array[b]
-    return array
-sorted_array = sort_array(unsorted_array)
+// Cool stuff
+while (!quit) {
+    var inputString = prompt("What do you want to do?");
+    var inputArray = inputString.split(" ");
+
+    if (typeof commandos[inputArray[0]] == "function") {
+        commandos[inputArray[0]].apply(commandos, inputArray);
+    }
+}
+
+false
