68c0d9c885e7a69ecc978978176692fd07abd86d
Filip Smets <alemaaltevinden@gmail.com>
2014-12-24 19:15:10 -0500

    "false"

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ while (!quit) {
     var inputString = prompt("What do you want to do?");
     var inputArray = inputString.split(" ");
 
-    if (typeof commandos[inputArray[0]] == "function") {
+    if (typeof commandos[inputArray[0]] === "function") {
         quit = commandos[inputArray[0]].apply(commandos, inputArray);
     }
 }
