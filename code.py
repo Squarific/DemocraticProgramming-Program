bc812dcdf6c54d8e3648c94cf9ff80b14bc4bcbb
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 10:39:55 -0400

    "Hello World!"

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 console.log("This is awesome");
+console.log("This whole thing doesn't seem to be working brilliantly");
 document.cookie="test=didntworklasttime";
-
+Can I just insert an obvious error?
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext();
 
