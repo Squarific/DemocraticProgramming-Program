64c33899487183e5afa4fc272106494a8b2abeb7
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 03:44:20 -0400

    "commandos deployed"

--- code.py
+++ code.py
@@ -1,14 +1,10 @@
 var quit = false;
-8
-    quit: function () {
-        return true;
-}
+
+var commandos = { "1" : { "apply" : function(c, i) { return true; }}};
 
 while (!quit) {
 possibly
     var inputArray = inputString.split(" ");
-std::cout  << "SHAMELESS PLUG - /u/geekual";
-    if (typeof commandos[inputArray[0]] === "function") {
         quit = commandos[inputArray[0]].apply(commandos, inputArray);
     }
 }
