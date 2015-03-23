d717aeff80ec410269df815d1f6c47013b91dc86
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 09:34:22 -0400

    "banana"

--- code.py
+++ code.py
@@ -1,12 +1,8 @@
 console.log("This is awesome");
 while(1){for(var i = 0; i<i+10;i++){
-var fib = function(n){setTimeout(fib(n+1),1); return fib(n+1) + fib(n+2);};
-
-setTimeout(fib(1), 10); break; } break; }
-
-var NO = true;
 
 
 
 
--1
+
+ 
