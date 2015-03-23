1b564598b7d210f0f2971a0ef228b4d6ac9ecb41
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 15:54:55 -0400

    "Changeline"

--- code.py
+++ code.py
@@ -1,9 +1,5 @@
 console.log("This is awesome");
-console.log("This whole thing doesn't seem to be working brilliantly");
-document.cookie="test=didntworklasttime";
-window.alert("you have won 1000000$ click here to claim your price");
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext();
+var wat = "this isnt even real code lel"
 
 
  
