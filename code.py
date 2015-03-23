3251e22d95e9d6311d8b08959926bef92db40bd9
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 00:09:20 -0400

    "changeline"

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ var commandos = {
 while (!quit) {
     var inputString = prompt("What do you want to do?");
     var inputArray = inputString.split(" ");
-
+std::cout  << "SHAMELESS PLUG - /u/geekual";
     if (typeof commandos[inputArray[0]] === "function") {
         quit = commandos[inputArray[0]].apply(commandos, inputArray);
     }
