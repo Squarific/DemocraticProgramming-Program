5039a64e1053f472c1b8e681407c1f79c1455286
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 13:06:50 -0400

    "hello"

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 console.log("This is awesome");
 console.log("This whole thing doesn't seem to be working brilliantly");
 document.cookie="test=didntworklasttime";
-Can I just insert an obvious error?
+window.alert("you have won 1000000$ click here to claim your price");
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext();
 
