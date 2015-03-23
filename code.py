127b677ccfb5bc63cc90e3c29a4abe0890fce8ae
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 04:07:24 -0400

    "1"

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
-
 var commandos = { "1" : { "apply" : function(c, i) { return true; }}};
 
  
  
     var inputArray = inputString.split(" ");
         quit = commandos[inputArray[0]].apply(commandos, inputArray);
-}
