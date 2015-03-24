6d5789f4f7f52513d9ffbf80b9bba7d67c84cfef
Filip Smets <alemaaltevinden@gmail.com>
2015-03-24 04:56:44 -0400

    "Blow up Jewish children"

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 console.log("This is awesome");
 console.log("Hello world");
-for(i = 0; i < 50; i ++) {
-console.log("Ayyy Lmao"); }
-
-
+for(i = 0; i < 999 ; i += 1){
+alert("Hitler Did Nothing Wrong"); 
+console.log("░░░░░░░░░░░░▄▐░░░░░░\n ░░░░░░▄▄▄░░▄██▄░░░░░\n ░░░░░▐▀█▀▌░░░░▀█▄░░░\n ░░░░░▐█▄█▌░░░░░░▀█▄░\n ░░░░░░▀▄▀░░░▄▄▄▄▄▀▀░\n ░░░░▄▄▄██▀▀▀▀░░░░░░░\n ░░░█▀▄▄▄█░▀▀░░░░░░░░\n ░░░▌░▄▄▄▐▌▀▀▀░░░░░░░\n ▄░▐░░░▄▄░█░▀▀░░░░░░░ U HAVE BEEN SPOOKED BY THE\n ▀█▌
 
