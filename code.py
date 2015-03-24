8593d6c93595358c36adb120fd66b62483e61c88
Filip Smets <alemaaltevinden@gmail.com>
2015-03-23 21:22:29 -0400

    "var swag = true"

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
 console.log("This is awesome");
-var wat = "this isnt even real code lel"
+var wat += "lololollolol";
 var rekt = "LOL, Run Code button is a parked website";
-window.location.href = "http://www.reddit.com/";
-
 
 
 console.log("wew");
