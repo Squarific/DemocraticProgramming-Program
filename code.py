3852ddb8356e43f3b2bb74d63a03b76eededb063
Filip Smets <alemaaltevinden@gmail.com>
2015-03-25 14:29:08 -0400

    "true"

--- code.py
+++ code.py
@@ -2,3 +2,7 @@ console.log("This is awesome");
 console.log("Hello world");
 console.log("░░░░░░░░░░░░▄▐░░░░░░\n ░░░░░░▄▄▄░░▄██▄░░░░░\n ░░░░░▐▀█▀▌░░░░▀█▄░░░\n ░░░░░▐█▄█▌░░░░░░▀█▄░\n ░░░░░░▀▄▀░░░▄▄▄▄▄▀▀░\n ░░░░▄▄▄██▀▀▀▀░░░░░░░\n ░░░█▀▄▄▄█░▀▀░░░░░░░░\n ░░░▌░▄▄▄▐▌▀▀▀░░░░░░░\n ▄░▐░░░▄▄░█░▀▀░░░░░░░ U HAVE BEEN SPOOKED BY THE\n ▀█▌
 alert("It's happening!");
+
+
+
+
