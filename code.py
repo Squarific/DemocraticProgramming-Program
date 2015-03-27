8b499626eb7f1535065c75725ee5e5c9e7530f03
Filip Smets <alemaaltevinden@gmail.com>
2015-03-27 16:00:48 -0400

    "test commit"

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
-console.log("This is awesome");
 console.log("Hello world");
+alert("Wykop kurwa");
 alert("I guess no one uses this anymore");
-alert("I guess no one uses this anymore");
-
+console.log("Why this stuff isn't working?");
 
