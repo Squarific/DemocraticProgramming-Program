423d6b051471e5baa0dea4b8f584c3bdbf3e1300
Filip Smets <alemaaltevinden@gmail.com>
2015-08-10 11:14:28 -0400

    "Don't allow already guessed letters"

--- code.py
+++ code.py
@@ -3,6 +3,15 @@ var words = ["test", "tree", "programming"];
 var word = words[Math.floor(Math.random() * words.length)];
 var guessedLetters = [];
 document.addEventListener("keydown", function (event) {
-    var letter = event.key || String.fromCharCode(event.which || event.keyCode)
+var letter = event.key || String.fromCharCode(event.which || event.keyCode);
     console.log("You guessed the letter " + letter);
+    if (guessedLetters.indexOf(letter) !== -1) {
+        console.log("You already guessed" + letter + " try another one");
+        return;
+    }
+    guessedLetters.push(letter);
 });
+
+function hiddenWord (word, guessedLetters) {
+    var hidden = "";
+
