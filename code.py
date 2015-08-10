a5dbb170b8996a3458866ed7d5be77273283b36f
Filip Smets <alemaaltevinden@gmail.com>
2015-08-10 13:48:50 -0400

    "Ẁ̵̶͇̥͇̭̫̗͔͓̹͔̺͉͙̜͉͡ͅͅh́͏̩̻̠̣̣͉̘a̵͖̣̟̙͔͎̭͙̜͕͚͎͙͚͖̪͟͜͞͠ţ̩͎͉̗̬̙̮͇̪͉̱̞̀́̕ͅ ̵̡͇͉̗̣̯͉͚̰̬͔̫̯́͢t̵̛̛̩̫̭̼̥̭̟̲ͅh҉̶̛̘̣͇̞̟̟̀ͅͅe̷̸̘̤̳͔͖̝͍͓̹͓̯̙̱̪ ̵̴̺̹̠̫̕ͅf̶̻͓͍̭͟ų̹͈̫̹͖̝̬͉͓̙͘͝͞c̷͟͏̦͎͙͓̝̣̱͕͖̲̱̦̜̖̝̳̘ķ͍͇͍̝̻͡ ̶̸̧̥͎̺͢͢i̷͢҉̞͉̞̩̝̥͈̱̙̲ͅs҉̡̲͈̯͙͚͝ ̵̶̧͙̰͍͇̬̤͘͠h̸̝̰͚̳̩̖͓̖̻̲̦̙̗̣̰̺͇̭̩̕à̵̲̣̻̺̣͚́p҉̯̘͍̳̩͚͕̭͙̠̙̠̬̲͘͝͞ͅp̸̧̡̻͔̫͙͎̫̹̬͖͚̖̮̲̬̀͢e͝͏͙̩̯̞̭̲͇͖͔͇͍̠͍n͜҉̠͎͈̺͔͙̲͔̣͉̺̫̖͝ͅí̸̳̞̰̦̖̺͎͍̳̤̩̲̫͙̝͉ͅͅn҉̶̬̼̻̣̣͍̥̫̲̝̰̲̀͠ͅg̴̲̜͇̲̫̘̙̦͇̼͜͟?̷̨̨͓̖̼͖̦͇͙̮̀"

--- code.py
+++ code.py
@@ -1,15 +1,21 @@
-console.log("You are playing hangman! Press letters that you think are in the word.");
-var words = ["test", "tree", "programming"];
-var word = words[Math.floor(Math.random() * words.length)];
-var guessedLetters = [];
+
+var word, guessedLetters;
+function start() {
+    console.log("You are playing hangman! Press letters that you think are in the word.");
+    var words = ['test', 'tree', 'programming', 'hash', 'fish', 'website', 'reddit', 'fun', 'sport', 'lol', 'bromance'];
+    word = words[Math.floor(Math.random() * words.length)];
+    guessedLetters = [];
+}
+
 document.addEventListener("keydown", function (event) {
-var letter = event.key || String.fromCharCode(event.which || event.keyCode);
+    var letter = event.key || String.fromCharCode(event.which || event.keyCode);
     console.log("You guessed the letter " + letter);
-    if (guessedLetters.indexOf(letter) !== -1) {
+    if (guessedLetters.indexOf(letter.toLowerCase()) !== -1) {
         console.log("You already guessed" + letter + " try another one");
         return;
     }
     guessedLetters.push(letter.toLowerCase());
+
     console.log(hiddenWord(word, guessedLetters));
 });
 
@@ -20,3 +26,9 @@ function hiddenWord (word, guessedLetters) {
     }
     return hidden;
 }
+
+
+
+
+
+console.log("H̴̸̘͇̟̥͔̤̻́̕͞e̷̢̖͈͖̖̼̰̟̠̫͍͈̩͙̘̻l̢̜̩͚̠͘͞p̡̛͏̴̩͎͉͈ ̴̣͈̱̻̹̪̰͞ͅm͍̭̙͙̫̹̣̕͘͟͡͡e̶͞҉͖̙̣͍̫̣̗̰e̴̸̙̠̳͕͖̜̞͓̜̱͓̪̕é̢̨̹̮̪̺̖̤̼̱͝ȩ̶̸͕̪̘̬͉̭̬͈̘̙̩͜");
