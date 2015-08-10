6f80389a357324cf4d6ee2eb165390605b310220
Filip Smets <alemaaltevinden@gmail.com>
2015-08-10 10:35:34 -0400

    "Select random word and display letters on keydown"

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
-console.log(1 + 1);
+console.log("You are playing hangman! Press letters that you think are in the word.");
+var words = ["test", "tree", "programming"];
+var word = words[Math.floor(Math.random() * words.length)];
+var guessedLetters = [];
 document.addEventListener("keydown", function (event) {
-    console.log(event);
+    var letter = event.key || String.fromCharCode(event.which || event.keyCode)
+    console.log("You guessed the letter " + letter);
 });
